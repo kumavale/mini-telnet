7f26d568968d1b607f2a0eca378c173f98e7f510
kumavale <kumavale.hyperion@gmail.com>
2023-09-06 06:37:50 +0900

    style: split into module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod telnet;
+
 use clap::Parser;
 use std::io::prelude::*;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
@@ -21,7 +23,7 @@ async fn main() -> anyhow::Result<()> {
     let (sender, receiver) = broadcast::channel(1);
     let (mut stream, mut sink) = stream.into_split();
 
-    negotiation(&mut stream, &mut sink).await?;
+    telnet::negotiation(&mut stream, &mut sink).await?;
 
     let input_handle = stdin(sender);
     let tx_handle = tx(sink, receiver);
@@ -61,54 +63,6 @@ async fn rx(mut stream: OwnedReadHalf) -> anyhow::Result<()> {
                 std::io::stdout().lock().write_all(&buf)?;
                 std::io::stdout().flush()?;
             }
-        }
-    }
-}
-
-async fn negotiation(stream: &mut OwnedReadHalf, sink: &mut OwnedWriteHalf) -> anyhow::Result<()> {
-    // options
-    const SUPPRESS_GO_AHEAD: u8 =  3;
-    const WINDOW_SIZE:       u8 = 31;
-    // commands
-    const SE:   u8 = 240;
-    const SB:   u8 = 250;
-    const WILL: u8 = 251;
-    const WONT: u8 = 252;
-    const DO:   u8 = 253;
-    const DONT: u8 = 254;
-    const IAC:  u8 = 255;
-
-    // My negotiation
-    sink.write_all(&[IAC, WILL, WINDOW_SIZE]).await?;
-
-    // Server negotiation
-    loop {
-        let mut buf = vec![0; 3];
-        match stream.peek(&mut buf).await {
-            Ok(0) => return Ok(()),
-            Ok(_) => {
-                if buf[0] == IAC {
-                    if buf[1] == DO {
-                        if buf[2] == WINDOW_SIZE {
-                            buf = vec![IAC, SB, WINDOW_SIZE, 0, 80, 0, 24, IAC, SE];
-                        } else {
-                            buf[1] = WONT
-                        }
-                    }
-                    if buf[1] == WILL {
-                        if buf[2] == SUPPRESS_GO_AHEAD {
-                            buf[1] = DO
-                        } else {
-                            buf[1] = DONT
-                        }
-                    }
-                    sink.write_all(&buf).await?;
-                    stream.read_exact(&mut buf).await?;
-                } else {
-                    return Ok(()); // End of Negotiation
-                }
-            }
-            Err(e) => anyhow::bail!(e),
         }
     }
 }
---
+++ src/telnet/mod.rs
@@ -0,0 +1,71 @@
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::tcp::{OwnedReadHalf, OwnedWriteHalf};
+
+/// Options
+#[rustfmt::skip]
+mod option {
+    pub const SUPPRESS_GO_AHEAD: u8 =  3;
+    pub const WINDOW_SIZE:       u8 = 31;
+}
+
+/// Commands
+#[rustfmt::skip]
+mod command {
+    pub const SE:   u8 = 240;
+    pub const SB:   u8 = 250;
+    pub const WILL: u8 = 251;
+    pub const WONT: u8 = 252;
+    pub const DO:   u8 = 253;
+    pub const DONT: u8 = 254;
+    pub const IAC:  u8 = 255;
+}
+
+pub async fn negotiation(
+    stream: &mut OwnedReadHalf,
+    sink: &mut OwnedWriteHalf,
+) -> anyhow::Result<()> {
+    // My init negotiation
+    sink.write_all(&[command::IAC, command::WILL, option::WINDOW_SIZE])
+        .await?;
+
+    // Server negotiation
+    loop {
+        let mut buf = vec![0; 3];
+        match stream.peek(&mut buf).await {
+            Ok(0) => return Ok(()),
+            Ok(_) => {
+                if buf[0] == command::IAC {
+                    if buf[1] == command::DO {
+                        if buf[2] == option::WINDOW_SIZE {
+                            buf = vec![
+                                command::IAC,
+                                command::SB,
+                                option::WINDOW_SIZE,
+                                0,
+                                80,
+                                0,
+                                24,
+                                command::IAC,
+                                command::SE,
+                            ];
+                        } else {
+                            buf[1] = command::WONT
+                        }
+                    }
+                    if buf[1] == command::WILL {
+                        if buf[2] == option::SUPPRESS_GO_AHEAD {
+                            buf[1] = command::DO
+                        } else {
+                            buf[1] = command::DONT
+                        }
+                    }
+                    sink.write_all(&buf).await?;
+                    stream.read_exact(&mut buf).await?;
+                } else {
+                    return Ok(()); // End of Negotiation
+                }
+            }
+            Err(e) => anyhow::bail!(e),
+        }
+    }
+}
