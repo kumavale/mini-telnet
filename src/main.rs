f5f2f43386425014ef66658a9a14be9d3d934b3f
kumavale <kumavale.hyperion@gmail.com>
2023-09-05 19:58:50 +0900

    feat: async tcp

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,48 @@
 use std::io::prelude::*;
-use std::net::TcpStream;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::tcp::{OwnedReadHalf, OwnedWriteHalf};
+use tokio::net::TcpStream;
+use tokio::sync::broadcast;
+use tokio::task::JoinHandle;
 
-fn main() -> std::io::Result<()> {
-    let mut stream = TcpStream::connect("127.0.0.1:2323")?;
+#[tokio::main]
+async fn main() {
+    let stream = TcpStream::connect("127.0.0.1:2323").await.unwrap();
+    let (sender, receiver) = broadcast::channel(1);
+    let (stream, sink) = stream.into_split();
 
-    loop {
+    let input_handle = stdin(sender);
+    let tx_handle = tx(sink, receiver);
+    let rx_handle = rx(stream);
+
+    tokio::select! {
+        _ = input_handle => (),
+        _ = tx_handle => (),
+        _ = rx_handle => (),
+    }
+}
+
+fn stdin(sender: broadcast::Sender<Vec<u8>>) -> JoinHandle<()> {
+    tokio::task::spawn_blocking(move || loop {
         let mut input = String::new();
-        std::io::stdin().read_line(&mut input)?;
-        stream.write_all(input.as_bytes())?;
+        std::io::stdin().read_line(&mut input).unwrap();
+        sender.send(input.into_bytes()).unwrap();
+    })
+}
 
-        let mut buf = [0; 128];
-        let n = stream.read(&mut buf)?;
-        std::io::stdout().write_all(&buf[..n])?;
+async fn tx(mut sink: OwnedWriteHalf, mut proxy: broadcast::Receiver<Vec<u8>>) {
+    loop {
+        let input = proxy.recv().await.unwrap();
+        sink.write_all(&input).await.unwrap();
+    }
+}
+
+async fn rx(mut stream: OwnedReadHalf) {
+    loop {
+        let mut buf = vec![];
+        match stream.read_buf(&mut buf).await {
+            Ok(0) | Err(_) => return,
+            Ok(_) => std::io::stdout().lock().write_all(&buf).unwrap(),
+        }
     }
 }
