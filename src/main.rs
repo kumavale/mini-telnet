9d42274c3038111be6868bd1f4a0ef14965c896c
kumavale <kumavale.hyperion@gmail.com>
2023-09-06 00:31:50 +0900

    feat: support telnet

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,11 @@ use tokio::task::JoinHandle;
 
 #[tokio::main]
 async fn main() {
-    let stream = TcpStream::connect("127.0.0.1:2323").await.unwrap();
+    let stream = TcpStream::connect("1984.ws:23").await.unwrap();
     let (sender, receiver) = broadcast::channel(1);
-    let (stream, sink) = stream.into_split();
+    let (mut stream, mut sink) = stream.into_split();
+
+    negotiation(&mut stream, &mut sink).await;
 
     let input_handle = stdin(sender);
     let tx_handle = tx(sink, receiver);
@@ -42,7 +44,57 @@ async fn rx(mut stream: OwnedReadHalf) {
         let mut buf = vec![];
         match stream.read_buf(&mut buf).await {
             Ok(0) | Err(_) => return,
-            Ok(_) => std::io::stdout().lock().write_all(&buf).unwrap(),
+            Ok(_) => {
+                std::io::stdout().lock().write_all(&buf).unwrap();
+                std::io::stdout().flush().unwrap();
+            }
+        }
+    }
+}
+
+async fn negotiation(stream: &mut OwnedReadHalf, sink: &mut OwnedWriteHalf) {
+    // options
+    const SUPPRESS_GO_AHEAD: u8 =  3;
+    const WINDOW_SIZE:       u8 = 31;
+    // commands
+    const SE:   u8 = 240;
+    const SB:   u8 = 250;
+    const WILL: u8 = 251;
+    const WONT: u8 = 252;
+    const DO:   u8 = 253;
+    const DONT: u8 = 254;
+    const IAC:  u8 = 255;
+
+    // My negotiation
+    sink.write_all(&[IAC, WILL, WINDOW_SIZE]).await.unwrap();
+
+    // Server negotiation
+    loop {
+        let mut buf = vec![0; 3];
+        match stream.peek(&mut buf).await {
+            Ok(0) | Err(_) => return,
+            Ok(_) => {
+                if buf[0] == IAC {
+                    if buf[1] == DO {
+                        if buf[2] == WINDOW_SIZE {
+                            buf = vec![IAC, SB, WINDOW_SIZE, 0, 80, 0, 24, IAC, SE];
+                        } else {
+                            buf[1] = WONT
+                        }
+                    }
+                    if buf[1] == WILL {
+                        if buf[2] == SUPPRESS_GO_AHEAD {
+                            buf[1] = DO
+                        } else {
+                            buf[1] = DONT
+                        }
+                    }
+                    sink.write_all(&buf).await.unwrap();
+                    stream.read_exact(&mut buf).await.unwrap();
+                } else {
+                    return; // End of Negotiation
+                }
+            }
         }
     }
 }
