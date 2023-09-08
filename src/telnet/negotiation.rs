e0310998c7d277b5ce2c13f5459cf1c672c5087a
kumavale <kumavale.hyperion@gmail.com>
2023-09-09 08:43:51 +0900

    style: 見た目

--- src/telnet/negotiation.rs
+++ src/telnet/negotiation.rs
@@ -17,35 +17,32 @@ pub async fn negotiation(
         let mut buf = vec![0; 3];
         match stream.peek(&mut buf).await? {
             0 => return Ok(()),
-            _ => {
-                if buf[0] == IAC {
-                    match buf[1] {
-                        WILL | DO | WONT | DONT => {
-                            debug_assert_eq!(buf.len(), 3);
-                            if buf[1] == DO {
-                                if buf[2] == WINDOW_SIZE {
-                                    buf = vec![IAC, SB, WINDOW_SIZE, 0, 80, 0, 24, IAC, SE];
-                                } else {
-                                    buf[1] = WONT;
-                                }
+            _ => match buf[0] {
+                IAC => match buf[1] {
+                    WILL | DO | WONT | DONT => {
+                        debug_assert_eq!(buf.len(), 3);
+                        if buf[1] == DO {
+                            if buf[2] == WINDOW_SIZE {
+                                buf = vec![IAC, SB, WINDOW_SIZE, 0, 80, 0, 24, IAC, SE];
+                            } else {
+                                buf[1] = WONT;
                             }
-                            if buf[1] == WILL {
-                                if buf[2] == SUPPRESS_GO_AHEAD {
-                                    buf[1] = DO;
-                                } else {
-                                    buf[1] = DONT;
-                                }
-                            }
-                            stream.read_exact(&mut [0; 3]).await?;
-                            sink.write_all(&buf).await?;
                         }
-                        SB => _ = stream.read_until(SE).await?,
-                        _ => unimplemented!(),
+                        if buf[1] == WILL {
+                            if buf[2] == SUPPRESS_GO_AHEAD {
+                                buf[1] = DO;
+                            } else {
+                                buf[1] = DONT;
+                            }
+                        }
+                        stream.read_exact(&mut [0; 3]).await?;
+                        sink.write_all(&buf).await?;
                     }
-                } else {
-                    return Ok(()); // End of Negotiation
-                }
-            }
+                    SB => _ = stream.read_until(SE).await?,
+                    _ => unimplemented!(),
+                },
+                _ => return Ok(()), // End of Negotiation
+            },
         }
     }
 }
